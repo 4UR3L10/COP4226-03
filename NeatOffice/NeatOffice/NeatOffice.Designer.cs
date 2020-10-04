d4a1f0713228a4036935346fd2d47252fd673662
Lyceum <48872358+cab-aranw@users.noreply.github.com>
2020-10-03 23:12:49 -0400

    REsize the Form NeatOffice

--- NeatOffice/NeatOffice/NeatOffice.Designer.cs
+++ NeatOffice/NeatOffice/NeatOffice.Designer.cs
@@ -35,7 +35,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1129, 692);
+            this.ClientSize = new System.Drawing.Size(953, 692);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "NeatOffice";
